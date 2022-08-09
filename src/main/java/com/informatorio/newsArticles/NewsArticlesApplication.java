1f9905c71f5b4c7758d35fadc77abef68823e02d
vmolinas <u.vmolinas@gmail.com>
2022-08-09 11:04:22 -0300

    fisrt commit

---
+++ src/main/java/com/informatorio/newsArticles/NewsArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.newsArticles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsArticlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/newsArticles/NewsArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.newsArticles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsArticlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
