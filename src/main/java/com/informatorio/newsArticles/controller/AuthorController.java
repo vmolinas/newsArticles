6f2e21c459be6ab6b56577befcc9d44bc95eab7e
vmolinas <u.vmolinas@gmail.com>
2022-08-27 12:21:53 -0300

    005 feat(api) get all authors change

--- src/main/java/com/informatorio/newsArticles/controller/AuthorController.java
+++ src/main/java/com/informatorio/newsArticles/controller/AuthorController.java
@@ -1,14 +1,20 @@
 package com.informatorio.newsArticles.controller;
 
 import com.informatorio.newsArticles.domain.Author;
+import com.informatorio.newsArticles.dto.AuthorPageDTO;
 import com.informatorio.newsArticles.repository.ArticleRepository;
 import com.informatorio.newsArticles.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @Validated
@@ -46,10 +52,21 @@ public class AuthorController {
 
     /*--CONSULTA(OBTENER TODOS LOS AUTORES)--*/
     @GetMapping("/author")
-    public List<Author> getAll() {
-        List<Author> authors = authorRepository.findAll();
-        return authors;
+    public ResponseEntity<?> findAll(@RequestParam(defaultValue = "0") @PositiveOrZero int page,
+                                     @RequestParam(defaultValue = "5") @Positive int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Author> authorPage = authorRepository.findAll(pageable);
+        AuthorPageDTO authorPageDTO = new AuthorPageDTO(authorPage.getNumber(),
+                authorPage.getSize(),
+                authorPage.getTotalElements(),
+                authorPage.getTotalPages());
+        return new ResponseEntity<>(authorPage, HttpStatus.OK);
     }
+//    @GetMapping("/author")
+//    public List<Author> getAll() {
+//        List<Author> authors = authorRepository.findAll();
+//        return authors;
+//    }
 
     /*--CONSULTA(OBTENER TODOS LOS AUTORES QUE CONTENGAN UN STRING BUSCADO EN EL FULLNAME)--*/
     @GetMapping("/author/{fullname}")
@@ -58,4 +75,5 @@ public class AuthorController {
     }
 
     /*--CONSULTA(OBTENER TODOS LOS AUTORES QUE FUERON CREADOS LUEGO DE UNA FECHA DADA)--*/
+
 }
